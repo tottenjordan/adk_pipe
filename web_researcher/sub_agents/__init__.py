f4da282bf182308494966014818a141bd0641f2f
Jordan <jordantotten@google.com>
2025-09-25 17:32:11 +0000

    deprecate web researcher and creative copy agents

--- web_researcher/sub_agents/__init__.py
+++
@@ -1,7 +0,0 @@
-from .trend_researcher.agent import gs_sequential_planner
-from .campaign_researcher.agent import ca_sequential_planner
-
-__all__ = [
-    "gs_sequential_planner",
-    "ca_sequential_planner",
-]
