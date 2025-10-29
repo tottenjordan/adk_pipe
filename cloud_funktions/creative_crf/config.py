512dca6a1faa01f83031b2607f8c802a6f5eeda7
Jordan <jordantotten@google.com>
2025-10-29 01:31:20 +0000

    update crf instructions and requirements

--- cloud_funktions/creative_crf/config.py
+++ cloud_funktions/creative_crf/config.py
@@ -7,6 +7,7 @@ class AppConfig:
     GOOGLE_CLOUD_PROJECT = "hybrid-vertex"
     GOOGLE_CLOUD_LOCATION = "us-central1"
     GOOGLE_CLOUD_PROJECT_NUMBER = 934903580331
+    CREATIVE_WORKER_TOPIC_NAME = "creative-worker-queue-topic"
 
 
 config = AppConfig()
